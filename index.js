67afd68f4d7705ed552ae16416bbe8947705e15a
Almatov Azamat <almatovalshin@gmail.com>
2022-12-21 17:37:42 +0500

    server structure created

--- index.js
+++ index.js
@@ -1,21 +1,53 @@
 const server = require('http').createServer()
 const io = require('socket.io')(server)
-var messages = [];
+var users = [];
+
+function removeUser(id) {
+    var index = users.findIndex(x => x.id === id);
+    if (index !== -1) {
+        users.splice(index, 1);
+    }
+}
 
 io.on("connection", (socket) => {
-    if(socket.handshake.query['platformToken'] != 'r43xv43vi') {
-        console.log(`${socket.id} disconnected`);
+    console.log(`${socket.id} trying to connect`);
+
+    if (socket.handshake.query['serverToken'] != 'r43xv43vi') {
+        console.log(`${socket.id} kicked, reason: invalid token`);
+        socket.emit("connectResult", 'invalid token');
         socket.disconnect();
+        return;
     }
 
-    console.log(socket.id);
-    console.log(socket.handshake.query['platformToken']);
-    socket.emit('messages_list', messages);
+    var index = users.findIndex(x => x.name === socket.handshake.query['userName']);
+    if (index !== -1) {
+        const anotherSocket = io.sockets.connected[users[index]['id']];
+        anotherSocket.emit("kicked", 'enabled another session');
+        anotherSocket.disconnect();
+    }
 
-    socket.on('message', function name(data) {
-        messages.push(data);
-        io.sockets.emit('messages_list', messages);
-    })
+    if (users.length >= 5) {
+        console.log(`${socket.id} kicked, reason: users limit`);
+        socket.emit("connectResult", 'users limit');
+        socket.disconnect();
+        return;
+    }
+
+    socket.emit("connectResult", true);
+    console.log(`${socket.id} connected`);
+    users.push({ 'id': socket.id, 'name': socket.handshake.query['userName'] });
+    console.log(users);
+
+    io.sockets.emit("usersList", users);
+
+    socket.on("disconnect", (reason) => {
+        console.log(`${socket.id} disconnected, reason: ${reason}`);
+        removeUser(socket.id);
+        console.log(users);
+
+        io.sockets.emit("usersList", users);
+    });
+
 });
 
 var server_port = process.env.PORT || 3000;

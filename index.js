0ce6b99b97cdbabbda17a4f717eaf41d0edb9c3b
Almatov Azamat <almatovalshin@gmail.com>
2022-12-22 14:50:40 +0500

    port opened

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const server = require('http').createServer()
-const io = require('socket.io')(server)
+const io = require('socket.io')(server,  { cors: { origin: '*', methods: ["GET", "POST"]} })
 var users = [];
 
 function removeUser(id) {
